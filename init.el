9ebff20edc0fced1c95a2d346c501e081154c513
Danie Roux <danie@danieroux.com>
2016-01-27 22:02:51 +0200

    This spacemacs thing. Let's try again, shall we?
    Org Agendas rendering correctly.

---
+++ init.el
@@ -0,0 +1,245 @@
+;; -*- mode: emacs-lisp -*-
+;; This file is loaded by Spacemacs at startup.
+;; It must be stored in your home directory.
+
+(defun dotspacemacs/layers ()
+  (setq-default
+   dotspacemacs-distribution 'spacemacs
+   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layers
+   '(
+     auto-completion
+     emacs-lisp
+     git
+     markdown
+     org
+     spell-checking
+     syntax-checking
+
+     djr-org
+     )
+   dotspacemacs-additional-packages '()
+   dotspacemacs-excluded-packages '()
+   dotspacemacs-delete-orphan-packages nil))
+
+(defun dotspacemacs/init ()
+  "Initialization function.
+This function is called at the very startup of Spacemacs initialization
+before layers configuration.
+You should not put any user code in there besides modifying the variable
+values."
+  (setq-default
+   dotspacemacs-check-for-update t
+   dotspacemacs-verbose-loading t
+   ;; List of items to show in the startup buffer. If nil it is disabled.
+   ;; Possible values are: `recents' `bookmarks' `projects'.
+   ;; (default '(recents projects))
+   dotspacemacs-startup-lists '(recents projects bookmarks)
+   dotspacemacs-startup-recent-list-size 10
+   dotspacemacs-themes '(zenburn)
+   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-leader-key "SPC"
+   dotspacemacs-major-mode-leader-key ","
+   dotspacemacs-emacs-leader-key "M-m"
+   dotspacemacs-editing-style 'vim
+   ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
+   ;; size to make separators look not too crappy.
+   dotspacemacs-default-font '("Monaco"
+                               :size 18
+                               :weight normal
+                               :width normal
+                               :powerline-scale 1.1)
+   dotspacemacs-command-key ":"
+   ;; Name of the default layout (default "Default")
+   dotspacemacs-default-layout-name "Default"
+   ;; If non nil the default layout name is displayed in the mode-line.
+   ;; (default nil)
+   dotspacemacs-display-default-layout nil
+   ;; If non nil then the last auto saved layouts are resume automatically upon
+   ;; start. (default nil)
+   dotspacemacs-auto-resume-layouts nil
+   ;; Location where to auto-save files. Possible values are `original' to
+   ;; auto-save the file in-place, `cache' to auto-save the file to another
+   ;; file stored in the cache directory and `nil' to disable auto-saving.
+   ;; (default 'cache)
+   dotspacemacs-auto-save-file-location 'cache
+   ;; Maximum number of rollback slots to keep in the cache. (default 5)
+   dotspacemacs-max-rollback-slots 5
+   ;; If non nil then `ido' replaces `helm' for some commands. For now only
+   ;; `find-files' (SPC f f), `find-spacemacs-file' (SPC f e s), and
+   ;; `find-contrib-file' (SPC f e c) are replaced. (default nil)
+   dotspacemacs-use-ido nil
+   ;; If non nil, `helm' will try to minimize the space it uses. (default nil)
+   dotspacemacs-helm-resize nil
+   ;; if non nil, the helm header is hidden when there is only one source.
+   ;; (default nil)
+   dotspacemacs-helm-no-header nil
+   ;; define the position to display `helm', options are `bottom', `top',
+   ;; `left', or `right'. (default 'bottom)
+   dotspacemacs-helm-position 'bottom
+   ;; If non nil the paste micro-state is enabled. When enabled pressing `p`
+   ;; several times cycle between the kill ring content. (default nil)
+   dotspacemacs-enable-paste-micro-state nil
+   ;; Which-key delay in seconds. The which-key buffer is the popup listing
+   ;; the commands bound to the current keystroke sequence. (default 0.4)
+   dotspacemacs-which-key-delay 0.4
+   ;; Which-key frame position. Possible values are `right', `bottom' and
+   ;; `right-then-bottom'. right-then-bottom tries to display the frame to the
+   ;; right; if there is insufficient space it displays it at the bottom.
+   ;; (default 'bottom)
+   dotspacemacs-which-key-position 'top
+   ;; If non nil a progress bar is displayed when spacemacs is loading. This
+   ;; may increase the boot time on some systems and emacs builds, set it to
+   ;; nil to boost the loading time. (default t)
+   dotspacemacs-loading-progress-bar t
+   ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
+   ;; (Emacs 24.4+ only)
+   dotspacemacs-fullscreen-at-startup nil
+   ;; If non nil `spacemacs/toggle-fullscreen' will not use native fullscreen.
+   ;; Use to disable fullscreen animations in OSX. (default nil)
+   dotspacemacs-fullscreen-use-non-native nil
+   ;; If non nil the frame is maximized when Emacs starts up.
+   ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
+   ;; (default nil) (Emacs 24.4+ only)
+   dotspacemacs-maximized-at-startup nil
+   ;; A value from the range (0..100), in increasing opacity, which describes
+   ;; the transparency level of a frame when it's active or selected.
+   ;; Transparency can be toggled through `toggle-transparency'. (default 90)
+   dotspacemacs-active-transparency 90
+   ;; A value from the range (0..100), in increasing opacity, which describes
+   ;; the transparency level of a frame when it's inactive or deselected.
+   ;; Transparency can be toggled through `toggle-transparency'. (default 90)
+   dotspacemacs-inactive-transparency 90
+   ;; If non nil unicode symbols are displayed in the mode line. (default t)
+   dotspacemacs-mode-line-unicode-symbols t
+   ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
+   ;; scrolling overrides the default behavior of Emacs which recenters the
+   ;; point when it reaches the top or bottom of the screen. (default t)
+   dotspacemacs-smooth-scrolling t
+   ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
+   ;; derivatives. If set to `relative', also turns on relative line numbers.
+   ;; (default nil)
+   dotspacemacs-line-numbers nil
+   ;; If non-nil smartparens-strict-mode will be enabled in programming modes.
+   ;; (default nil)
+   dotspacemacs-smartparens-strict-mode nil
+   ;; Select a scope to highlight delimiters. Possible values are `any',
+   ;; `current', `all' or `nil'. Default is `all' (highlight any scope and
+   ;; emphasis the current one). (default 'all)
+   dotspacemacs-highlight-delimiters 'all
+   ;; If non nil advises quit functions to keep server open when quitting.
+   ;; (default nil)
+   dotspacemacs-persistent-server nil
+   ;; List of search tool executable names. Spacemacs uses the first installed
+   ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
+   ;; (default '("ag" "pt" "ack" "grep"))
+   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   ;; The default package repository used if no explicit repository has been
+   ;; specified with an installed package.
+   ;; Not used for now. (default nil)
+   dotspacemacs-default-package-repository nil
+   ;; Delete whitespace while saving buffer. Possible values are `all'
+   ;; to aggressively delete empty line and long sequences of whitespace,
+   ;; `trailing' to delete only the whitespace at end of lines, `changed'to
+   ;; delete only whitespace for changed lines or `nil' to disable cleanup.
+   ;; (default nil)
+   dotspacemacs-whitespace-cleanup 'trailing
+   ))
+
+(defun dotspacemacs/user-init ()
+  (when (equal system-type 'darwin)
+    (setq *osx* t)
+    (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend)))
+
+(defun dotspacemacs/user-config ()
+  "Configuration function for user code.
+This function is called at the very end of Spacemacs initialization after
+layers configuration. You are free to put any user code."
+
+  (setq powerline-default-separator 'nil))
+
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#3E3D31" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#3E3D31" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(package-selected-packages
+   (quote
+    (helm-company helm-c-yasnippet company-statistics company-quickhelp company auto-yasnippet ac-ispell xterm-color toc-org smeargle shell-pop orgit org-repo-todo org-present org-pomodoro org-plus-contrib org-bullets multi-term mmm-mode markdown-toc markdown-mode magit-gitflow htmlize helm-gitignore helm-flyspell gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger gh-md flycheck-pos-tip flycheck evil-magit eshell-prompt-extras esh-help auto-dictionary zenburn-theme monokai-theme solarized-theme window-numbering volatile-highlights vi-tilde-fringe spaceline smooth-scrolling rainbow-delimiters persp-mode pcre2el paradox open-junk-file neotree move-text lorem-ipsum linum-relative leuven-theme info+ indent-guide hungry-delete highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-mode-manager helm-make helm-ag google-translate golden-ratio flx-ido fancy-battery expand-region evil-tutor evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-jumper evil-indent-plus evil-iedit-state evil-exchange evil-args evil-anzu eval-sexp-fu define-word clean-aindent-mode buffer-move bracketed-paste auto-highlight-symbol aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ws-butler which-key use-package spacemacs-theme restart-emacs quelpa popwin page-break-lines macrostep ido-vertical-mode hl-todo help-fns+ helm-projectile helm-flx helm-descbinds fill-column-indicator exec-path-from-shell evil-visualstar evil-surround evil-escape elisp-slime-nav bind-map auto-compile)))
+ '(pos-tip-background-color "#A6E22E")
+ '(pos-tip-foreground-color "#272822")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3E3D31" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ layers/djr-org/packages.el
@@ -0,0 +1,263 @@
+(defun djr-org/post-init-org ()
+  (org-clock-persistence-insinuate)
+
+  (setq org-modules (quote (org-habit)))
+
+  (use-package org-mac-link)
+
+  ;; Files
+  (setq gtd-file "~/Dropbox/Documents/gtd/gtd.org.gpg"
+        consulting-file "~/Dropbox/Documents/consulting/consulting.org.gpg"
+        inbox-file "~/Dropbox/Documents/gtd/inbox.org"
+        someday-file "~/Dropbox/Documents/gtd/someday_maybe.org.gpg"
+        brain-file "~/Dropbox/Documents/brain/brain.org.gpg"
+        conversations-file "~/Dropbox/Documents/gtd/conversations.org"
+        period-log-file "~/Dropbox/Documents/journal/period.org.gpg"
+        blog-ideas-file "~/Dropbox/Documents/gtd/blog_ideas.org.gpg")
+
+  (setq org-agenda-files `( ;; "~/Dropbox/Documents"
+                           ;; "~/Dropbox/Documents/gtd"
+                           ;; ,brain-file
+                           ,gtd-file
+                           ,consulting-file
+                           ))
+
+  (setq org-directory "~/Dropbox/Documents/gtd")
+
+  (setq org-blank-before-new-entry nil
+        org-enforce-todo-dependencies t
+        org-fast-tag-selection-include-todo t
+        org-fast-tag-selection-single-key t
+        org-use-fast-todo-selection t
+        org-treat-S-cursor-todo-selection-as-state-change nil
+        org-hide-leading-stars t
+        org-agenda-skip-additional-timestamps-same-entry nil
+        org-id-method (quote uuidgen)
+                                        ; Make clocktable indents pretty-ish
+        org-pretty-entities t
+        org-id-link-to-org-use-id t)
+
+  ;; Todo config
+  (setq org-todo-keywords (quote ((sequence "NEXT(n)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(z) | NOTE(t)"))))
+
+  (setq djr-single-task-header-id "C3478345-CEEF-497D-97EF-32AB278FBCF3")
+
+  (setq org-capture-templates `(("P" "New Project" entry (file ,gtd-file) "* %^{Project name}
+** NEXT %^{First task}%?")
+                                ("b" "Brain" entry (file ,brain-file) "* %?
+  %u
+
+%a")
+                                ("l" "liam" entry (file "~/Dropbox/Documents/liam.org") "* %?" :clock-in t :clock-resume t)
+                                ("f" "Fieldstone" entry (file "~/Dropbox/Documents/gtd/stones.org") "* %?
+  %u
+
+%a")
+                                ("i" "inbox" entry (file ,inbox-file) "* NEXT %?
+  %u
+
+%a")
+                                ("S" "Someday/Maybe" entry (file ,someday-file) "* NEXT %?
+  %u
+
+%a")
+                                ("I" "Pay Invoice" entry (id ,djr-single-task-header-id) "* NEXT %a :@banking:
+  %u
+
+%a")
+                                ("R" "Read paper" entry (id "54B79B33-F158-4200-A317-83DE22D6E6B6") "* NEXT %a :@open:
+  %u
+
+%a")
+                                ("s" "Single task" entry (id ,djr-single-task-header-id) "* NEXT %? %^g
+  %u
+
+%a")
+                                ("e" "Follow up email" entry (id ,djr-single-task-header-id) "* NEXT %? %a                     :@online:
+  %u
+  SCHEDULED: %^t
+
+%a")
+                                ("n" "note" entry (file ,inbox-file) "* NOTE %?
+  %u
+%a")
+
+                                ("d" "daily" entry (file ,period-log-file) "* %U
+
+%?")
+                                ("D" "dream" entry (file "~/Dropbox/Documents/journal/dream.org.gpg") "* %U
+
+%?")
+                                ("c" "The current Chrome tab" entry (file ,inbox-file) "* NEXT %? %(org-mac-chrome-get-frontmost-url)  :@online:
+  %u
+
+%a")))
+
+  (setq org-stuck-projects
+        '("+LEVEL=1+project-persistent/-DONE-CANCELLED" ("NEXT" "STARTED") ()))
+
+  (setq org-tag-alist (quote ((:startgroup)
+                              ("@errands" . ?e)
+                              ("@banking" . ?b)
+                              ("@calls" . ?c)
+                              ("@home" . ?h)
+                              ("@open" . ?O)
+                              ("@notebook" . ?a)
+                              ("@online" . ?o)
+                              ("@agenda" . ?A)
+                              ("@watch" . ?W)
+                              (:endgroup)
+                              ("crypt" . ?s)
+                              ("drill" . ?D)
+                              ("project" . ?P))))
+
+  (setq org-agenda-tags-todo-honor-ignore-options t
+        org-agenda-todo-ignore-scheduled 'future
+        org-agenda-skip-function-global nil)
+
+  (setq org-agenda-sorting-strategy
+        '(time-up category-keep priority-down))
+
+  (defun gtd-refile ()
+    "Includes email and anything in an inbox"
+    `(tags "refile" ((org-agenda-overriding-header "Inbox"))))
+
+  (defun gtd-agenda ()
+    `(agenda "-MAYBE" ((org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo '("NEXT" "STARTED" "WAITING" "project"))))))
+
+  (defun gtd-context (heading query-string)
+    `(tags-todo ,(concat query-string "/!-DONE-CANCELLED-WAITING")
+                ((org-agenda-overriding-header ,heading)
+                 (org-tags-match-list-sublevels t)
+                 (org-agenda-tags-todo-honor-ignore-options t)
+                 (org-agenda-todo-ignore-scheduled 'future))))
+
+  (defun gtd-project-context (tag &optional if-mode)
+    (gtd-context tag (concat "+project+" tag if-mode)))
+
+  (defun gtd-agenda-entry (key context-tag)
+    `((,key ,context-tag ,@(gtd-project-context context-tag))))
+
+  (setq org-agenda-custom-commands `(("H" "@home"
+                                      (,(gtd-agenda)
+                                       ,(gtd-project-context "@banking" "-consulting")
+                                       ,(gtd-project-context "@home" "-consulting")
+                                       ,(gtd-project-context "@open" "-consulting")
+                                       ,(gtd-project-context "@online" "-consulting")
+                                       ,(gtd-project-context "@notebook" "-consulting")
+                                       ,(gtd-project-context "@calls" "-consulting")
+                                       ,(gtd-project-context "@watch")
+                                       ,(gtd-refile)))
+                                     ("N" "@notebook"
+                                      (,(gtd-agenda)
+                                       ,(gtd-project-context "@banking")
+                                       ,(gtd-project-context "@notebook")
+                                       ,(gtd-project-context "@calls")
+                                       ,(gtd-project-context "@open")
+                                       ,(gtd-project-context "@online")
+                                       ,(gtd-project-context "@errands")
+                                       ,(gtd-project-context "@watch")
+                                       ,(gtd-refile)))
+                                     ("M" "Mobile"
+                                      (,(gtd-agenda)
+                                       ,(gtd-project-context "@open")
+                                       ,(gtd-project-context "@calls")
+                                       ,(gtd-project-context "@errands")
+                                       ,(gtd-project-context "@online")
+                                       ,(gtd-project-context "@banking")))
+                                     ,@(gtd-agenda-entry "n" "@notebook")
+                                     ,@(gtd-agenda-entry "e" "@errands")
+                                     ,@(gtd-agenda-entry "o" "@online")
+                                     ,@(gtd-agenda-entry "O" "@open")
+                                     ,@(gtd-agenda-entry "A" "@agenda")
+                                     ,@(gtd-agenda-entry "h" "@home")
+                                     ,@(gtd-agenda-entry "b" "@banking")
+                                     ,@(gtd-agenda-entry "c" "@calls")
+                                     ,@(gtd-agenda-entry "W" "@watch")
+                                     ("w" "Waiting" todo "WAITING" ((org-agenda-overriding-header "Waiting")))
+                                     ("r" "refile" tags "refile" nil)
+                                     ("p" "projects" tags "+LEVEL=1+project-persistent-@agenda-MAYBE/-CANCELLED-DONE" nil)
+                                     ("E" "Todo items without context (in error)"
+                                      ((tags "+project+TODO=\"NEXT\"-{@.*}"))
+                                      ((org-agenda-overriding-header "context free")))))
+
+  (defun djr/org-mode-refile-current-task-as-single-task ()
+    "Refiles the current task as a single task in gtd.org"
+    (interactive)
+    (let* ((org-refile-targets `((,gtd-file . (:tag . "single"))))
+           ;; Because the tag captures the top level to, grab the second entry
+           (rfloc (nth 1 (org-refile-get-targets))))
+      (org-refile nil nil rfloc)))
+
+
+  (setq org-completion-use-ido t
+        org-refile-targets `((,(remove brain-file org-agenda-files) :level . 1)
+                             (,brain-file . (:level . 0))
+                             (,period-log-file . (:level . 0))
+                             (,someday-file . (:level . 0))
+                             (,blog-ideas-file . (:level . 0))
+                             (,conversations-file . (:level . 1))
+                             (nil . (:level . 1)))
+        org-refile-use-outline-path (quote file)
+        org-outline-path-complete-in-steps nil
+        org-refile-allow-creating-parent-nodes nil)
+
+
+  (setq org-agenda-include-diary nil)
+  (setq org-agenda-todo-ignore-scheduled 'future)
+
+  (setq org-agenda-span 'day
+        org-deadline-warning-days 5)
+
+  (setq org-cycle-separator-lines 0
+        org-log-done 'time)
+
+  (setq org-src-fontify-natively t)
+  (setq org-archive-default-command (quote org-archive-to-archive-sibling)
+        org-archive-location "%s_archive.gpg::")
+
+  ;; Dim blocked tasks
+  (setq org-agenda-dim-blocked-tasks 'invisible)
+
+  ;; Compact the block agenda view
+  (setq org-agenda-compact-blocks t)
+
+  (setq org-enforce-todo-checkbox-dependencies t
+        org-enforce-todo-dependencies t)
+
+  (setq org-use-speed-commands t
+        ;; Also see org-speed-commands-default
+        org-speed-commands-user (quote (("0" . ignore)
+                                        ("1" . ignore) ;; For org-drill
+                                        ("2" . ignore)
+                                        ("3" . ignore)
+                                        ("4" . ignore)
+                                        ("5" . ignore)
+                                        ("d" . org-decrypt-entry)
+                                        ("j" . ignore)
+                                        ("J" . org-clock-goto)
+                                        ("k" . ignore)
+                                        ("K" . org-cut-special)
+                                        ("N" . org-narrow-to-subtree)
+                                        ("P" . org-pomodoro)
+                                        ("q" . djr/show-org-agenda-refreshing-if-empty)
+                                        ("s" . djr/org-mode-refile-current-task-as-single-task)
+                                        ("z" . org-add-note)
+                                        ("W" . widen))))
+
+  (setq org-agenda-persistent-filter t)
+  (setq org-clock-idle-time 5)
+
+  (defun djr/show-org-agenda-refreshing-if-empty ()
+    "If the Org Agenda buffer has been drawn, show it. Else refresh and show."
+    (interactive)
+    (if (or (not (get-buffer "*Org Agenda*"))
+            (= 0 (buffer-size (get-buffer "*Org Agenda*"))))
+        (djr/agenda-notebook))
+    (switch-to-buffer "*Org Agenda*")
+    (delete-other-windows)))
+
+(defconst djr-org-packages
+  '(org (org-crypt :location built-in)))
+
+;;; packages.el ends here
