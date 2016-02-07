c943c8016a29a4b96c7ccfd91de2c9b39a1fb1fc
Danie Roux <danie@danieroux.com>
2016-02-07 13:57:15 +0200

    Tinker some more with the modeline

--- djr-config.el
+++ djr-config.el
@@ -1,37 +1,48 @@
 (use-package shell)
 (djr/sync-mail-and-update-mu4e)
 
+(setq ns-use-srgb-colorspace nil)
 (setq powerline-default-separator nil
       spaceline-pre-hook 'nil ; Does diminish work, only
       )
 
-(setq ns-use-srgb-colorspace nil)
+(spaceline-toggle-line-column-off)
+(spaceline-toggle-buffer-encoding-abbrev-off)
 
-(make-face 'mode-line-directory-face)
-(set-face-attribute 'mode-line-directory-face nil
-                    :inherit 'mode-line-face)
+;; I want it to stand out, slightly. Could not get a useful face from powerline
+;; for this
 (make-face 'mode-line-filename-face)
 (set-face-attribute 'mode-line-filename-face nil
                     :inherit 'mode-line-face
                     :foreground "#eab700"
                     :weight 'bold)
 
+(defface djr-filename-active-face
+  '((t (:inherit 'powerline-active1 :foreground "#eab700")))
+  "Active filename"
+  :group 'powerline)
+
 (spaceline-define-segment djr-buffer-name
   "My custom way of presenting files"
-  (cond
-   ((buffer-file-name)
-    (format "%s%s"
-     (propertize (abbreviate-file-name (file-name-directory (buffer-file-name)))
-                 'face 'mode-line-directory-face)
-     (propertize (file-name-nondirectory (buffer-file-name)) 'face 'mode-line-filename-face)))
-   (t
-    (propertize "%b" 'face 'mode-line-filename-face)))
-  )
+  (let* ((active (powerline-selected-window-active))
+         (directory-face (spaceline--get-face 'face1 active))
+         (filename-face (if active
+                            'djr-filename-active-face
+                          directory-face)))
+    (cond
+     ((buffer-file-name)
+      (format "%s%s"
+              (propertize (abbreviate-file-name (file-name-directory (buffer-file-name)))
+                          'face directory-face)
+              (propertize (file-name-nondirectory (buffer-file-name)) 'face filename-face)))
+     (t
+      (propertize "%b" 'face 'mode-line-filename-face)))))
 
 (setq djr-spaceline-left
       '(((evil-state) :separator "|" :face highlight-face)
         anzu auto-compile
-        (buffer-modified djr-buffer-name remote-host)
+        (buffer-modified remote-host)
+        (djr-buffer-name :skip-alternate t)
         major-mode
         ((flycheck-error flycheck-warning flycheck-info)
          :when active)
