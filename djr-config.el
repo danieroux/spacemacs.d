965eae3a0d74e410a114c38a6cf75873f9da3702
Danie Roux <danie@danieroux.com>
2016-02-07 11:48:14 +0200

    Switch off many superfluous segments on the modeline

--- djr-config.el
+++ djr-config.el
@@ -1,6 +1,15 @@
 (use-package shell)
 (djr/sync-mail-and-update-mu4e)
 
-(setq powerline-default-separator 'nil)
+(setq powerline-default-separator nil
+      spaceline-pre-hook 'nil ; Does diminish work, only
+      )
+
+(setq ns-use-srgb-colorspace nil)
+
+(spaceline-toggle-minor-modes-off)
+(spaceline-toggle-line-column-off)
+(spaceline-toggle-buffer-size-off)
+(spaceline-toggle-buffer-encoding-abbrev-off)
 
 (provide 'djr-config)
