9866c83aa38ca9c597ae4f5784ee0be2b202b28a
Danie Roux <danie@danieroux.com>
2016-02-07 13:26:36 +0200

    Port my directory/file modeline, roughly

--- djr-config.el
+++ djr-config.el
@@ -7,9 +7,38 @@
 
 (setq ns-use-srgb-colorspace nil)
 
-(spaceline-toggle-minor-modes-off)
-(spaceline-toggle-line-column-off)
-(spaceline-toggle-buffer-size-off)
-(spaceline-toggle-buffer-encoding-abbrev-off)
+(make-face 'mode-line-directory-face)
+(set-face-attribute 'mode-line-directory-face nil
+                    :inherit 'mode-line-face)
+(make-face 'mode-line-filename-face)
+(set-face-attribute 'mode-line-filename-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#eab700"
+                    :weight 'bold)
+
+(spaceline-define-segment djr-buffer-name
+  "My custom way of presenting files"
+  (cond
+   ((buffer-file-name)
+    (format "%s%s"
+     (propertize (abbreviate-file-name (file-name-directory (buffer-file-name)))
+                 'face 'mode-line-directory-face)
+     (propertize (file-name-nondirectory (buffer-file-name)) 'face 'mode-line-filename-face)))
+   (t
+    (propertize "%b" 'face 'mode-line-filename-face)))
+  )
+
+(setq djr-spaceline-left
+      '(((evil-state) :separator "|" :face highlight-face)
+        anzu auto-compile
+        (buffer-modified djr-buffer-name remote-host)
+        major-mode
+        ((flycheck-error flycheck-warning flycheck-info)
+         :when active)
+        (version-control :when active)
+        (org-pomodoro :when active)
+        (org-clock :when active)))
+
+(spaceline-install djr-spaceline-left spaceline-right)
 
 (provide 'djr-config)
